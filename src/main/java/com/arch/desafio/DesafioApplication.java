41dc202c24925829b218cba8b643f86f09ba6b99
anisberto dos reis do nascimento <anisbertoos@gmail.com>
2024-05-01 22:31:05 -0300

    feat: bootstrap do projeto com as principais dependencias

---
+++ src/main/java/com/arch/desafio/DesafioApplication.java
@@ -0,0 +1,16 @@
+package com.arch.desafio;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de Serviços e Login", description = "Esta API é usada para cadastro de serviços e login.", version = "1.0"))
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
